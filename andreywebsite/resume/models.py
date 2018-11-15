fe539b38678c681bc2148208cf74a603e7ef6c95
andreyvarlamov <andreyvarlamov@github.com>
2018-11-15 09:34:02 -0800

    improved models; created simple view; migrations; admin setup

--- andreywebsite/resume/models.py
+++ andreywebsite/resume/models.py
@@ -3,27 +3,24 @@ from django.db import models
 # Activity and its children
 
 class Activity(models.Model):
-	pub_date = models.DateTimeField('date published')
-	#start_date = models.DateField('date activity started')
-	#end_date = models.DateField('date activity ended')
+	pub_date = models.DateTimeField('date publishesd')
+
 	name = models.CharField(max_length = 200)
-	#details = models.CharField(max_length = 200)
+	secName = models.CharField(max_length = 200)
+	# TO DO: make this an enum or something
+	# for now just char field - "Coop Work Experience", "Technical Project", "Volunteer Experience", "Education", "Professional Affiliation"
+	activityType = models.CharField(max_length = 200)
+
+	start_date = models.DateField('date activity started')
+	end_date = models.DateField('date activity ended')
+	
 	attachments = models.CharField(max_length = 200)
 
-class CoopActivity(Activity):
-	pass
 
-class TechnicalProjectActivity(Activity):
-	pass
-
-class VolunteerWorkActivity(Activity):
-	pass
-
-class EducationActivity(Activity):
-	pass
-
-class AffiliationActivity(Activity):
-	pass
+class ActivityDetail(models.Model):
+	pub_date = models.DateTimeField('date published')
+	detail = models.CharField(max_length = 200)
+	activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
 
 
 # Other Models
--- andreywebsite/resume/views.py
+++ andreywebsite/resume/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from django.views import generic
 from django.http import HttpResponse
+
+from .models import Activity
 
 # Create your views here.
 
-def index(request):
-	return HttpResponse('test')
+class IndexView(generic.ListView):
+	template_name = "resume/index.html"
+	context_object_name = "all_activities"
+	
+	def get_queryset(self):
+		return Activity.objects.all()
